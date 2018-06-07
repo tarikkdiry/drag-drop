a9b4114ae6ee6ad0ca8d3a3081f8ac5474a1038a
Tarik Kdiry <tarik.kdiry@gmail.com>
2018-06-07 16:18:01 -0400

    remove opening view

--- src/app/dnd/dnd.component.ts
+++ src/app/dnd/dnd.component.ts
@@ -10,12 +10,11 @@ import * as FileSaver from 'file-saver';
 export class DndComponent {
   url: any;
   public show = false; // Boolean for views
-  public open = true;
   public buttonName: any = 'Upload'; // Default name for button
   public back = true; // back button
   public upload = false; // upload boolean
   private fileList: any = []; // initialize array of file names
-  private showCaptionField = true; // caption field boolean
+  private showCaptionField = false; // caption field boolean
   public imageCount = 0; // Counts the amount of images being uploaded
 
   caption = '';
@@ -24,23 +23,7 @@ export class DndComponent {
     Triggered when buttons are pressed, responsible for changing the button titles
   */
   toggle() {
-    this.show = !this.show;
     this.showCaptionField = false;
-
-    if (this.show) {
-      this.buttonName = 'Back';
-    } else {
-      this.buttonName = 'Upload';
-    }
-
-    if (this.open) {
-      this.open = !this.open;
-      this.back = true;
-      this.buttonName = 'Back';
-    } else {
-      this.back = false;
-      this.open = true;
-    }
 
     if (this.upload === true) {
       this.upload = true;
@@ -85,24 +68,4 @@ export class DndComponent {
   addText(title: string) {
     console.log(title);
   }
-
-  // tslint:disable-next-line:member-ordering
-  customStyle = {
-    selectButton: {
-      'color': 'white',
-      'background-color': 'purple',
-    },
-    clearButton: {
-      'color': 'white',
-      'background-color': 'yellow',
-    },
-    layout: {
-      'background-color': 'black',
-      'color': 'red',
-      'font-size': '15px',
-    },
-    previewPanel: {
-      'background-color': 'red',
-    }
-  };
 }
